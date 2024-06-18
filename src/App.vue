846714de23c3c23cc8e47fb2bd90488035809bfc
Lee <lyg323323@gmail.com>
2024-06-18 17:52:48 +0900

    ref vs reactive

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,55 @@
 <template>
-  <div>{{ name }}</div>
+  <p>{{ name1 }}</p>
+  <p>{{ name2 }}</p>
+  <p>{{ name3 }}</p>
+
+  <button class="btn btn-primary" v-on:click="updateName">Click</button>
 </template>
 
 <script>
+import { ref, reactive } from 'vue';
+
 export default {
   setup() {
-    const name = 'Vue3 Project'
+    // 함수
+    // const name = 'test';
+    // const greeting = (name) => {
+    //   return 'Hello, ' + name
+    // }
+    // const greet = greeting(name)
+
+    // return {
+    //   greet
+    // }
+
+    // ref, reactive
+    const name1 = ref('test');
+    const name2 = ref({
+      id: 1
+    });
+    const name3 = reactive({
+      id: 1
+    });
+
+    const updateName = () => {
+      // ref : number, string
+      name1.value = 'test test';
+
+      // ref : object, array
+      name2.value.id = '2';
+
+      // reactive : object, array
+      name3.id = '2';
+    };
 
     return {
-      name
-    }
+      name1,
+      name2,
+      name3,
+      updateName
+    };
   }
-}
+};
 </script>
 
 <style></style>
