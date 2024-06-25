b45ed626203e30fccb353ed4613bd0ff25b12ac4
Lee <lyg323323@gmail.com>
2024-06-25 19:07:23 +0900

    async/await

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="container">
-    <h1>To-Do List</h1>
-
+    <h2>To-Do List</h2>
+    <input class="form-control" type="text" v-model="searchText" placeholder="Search" />
+    <hr />
     <TodoSimpleForm @add-todo="addTodo" />
-    <div v-if="!todos.length">추가된 Todo가 없습니다</div>
+    <div>{{ error }}</div>
 
-    <TodoList :todos="todos" @toggle-todo="toggleTodo" @delete-todo="deleteTodo" />
+    <div v-if="!filteredTodos.length">There is nothing to display</div>
+    <TodoList :todos="filteredTodos" @toggle-todo="toggleTodo" @delete-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 import TodoList from './components/TodoList.vue';
+import axios from 'axios';
 
 export default {
   components: {
@@ -21,14 +24,40 @@ export default {
   },
   setup() {
     const todos = ref([]);
-    const todoStyle = {
-      textDecoration: 'line-through',
-      color: 'gray'
+    const error = ref('');
+
+    // async/await: 비동기 함수
+    const addTodo = async (todo) => {
+      error.value = '';
+      try {
+        const res = await axios.post('http://localhost:3000/todos', {
+          subject: todo.subject,
+          completed: todo.completed
+        });
+        todos.value.push(res.data);
+      } catch (err) {
+        console.log(err);
+        error.value = 'Something went wrong.';
+      }
     };
 
-    const addTodo = (todo) => {
-      todos.value.push(todo);
-    };
+    // 데이터베이스 투두를 저장
+    // const addTodo = (todo) => {
+    //   error.value = '';
+    //   axios
+    //     .post('http://localhost:3000/todos', {
+    //       subject: todo.subject,
+    //       completed: todo.completed
+    //     })
+    //     .then((res) => {
+    //       console.log(res);
+    //       todos.value.push(res.data);
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //       error.value = 'Something went wrong.';
+    //     });
+    // };
 
     const deleteTodo = (index) => {
       todos.value.splice(index, 1);
@@ -38,12 +67,24 @@ export default {
       todos.value[index].completed = !todos.value[index].completed;
     };
 
+    const searchText = ref('');
+    const filteredTodos = computed(() => {
+      if (searchText.value) {
+        return todos.value.filter((todo) => {
+          return todo.subject.includes(searchText.value);
+        });
+      }
+      return todos.value;
+    });
+
     return {
       todos,
-      todoStyle,
       addTodo,
       deleteTodo,
-      toggleTodo
+      toggleTodo,
+      searchText,
+      filteredTodos,
+      error
     };
   }
 };
